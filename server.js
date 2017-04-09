55d1ff73a9a3dcd07cd69e4b45b63e334a9b7cfa
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-08 17:24:32 -0700

    add users

--- server.js
+++ server.js
@@ -20,13 +20,31 @@ io.sockets.on('connection', function (socket) {
 
     // Disconnect
     socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s socket connected', connections.length);
     });
 
     // Send message
-    socket.on('send message', function(data){
+    socket.on('send message', function (data) {
         console.log(data);
-        io.sockets.emit('new message', {msg: data})
+        io.sockets.emit('new message', {
+            msg: data
+        })
     });
+
+    //new user
+    socket.on('new user', function (data, callback) {
+        console.log(data);
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
 });
